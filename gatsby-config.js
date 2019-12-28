127c03d83a66a03d98a2e93ffcabad16758615bc
Misha Balin <m.balin@icloud.com>
2019-12-29 00:30:26 +0300

    add typescript support

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-typescript",
     {
       resolve: "gatsby-plugin-mdx",
       options: {
