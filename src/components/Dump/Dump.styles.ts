e08794f2b44455e9eeea5f04829e31aeb2bc97d1
Misha Balin <m.balin@icloud.com>
2019-12-31 00:00:25 +0300

    update Dump component

--- src/components/Dump/Dump.styles.ts
+++ src/components/Dump/Dump.styles.ts
@@ -1,8 +1,29 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  fontsize: 20;
-  border: "1px solid #efefef";
-  padding: 10;
-  background: "white";
+  font-size: 1rem;
+  border: 1px solid #efefef;
+  padding: 0.5rem;
+  background-color: #2c2c2c;
+  color: #e0e0e0;
+  overflow-x: auto;
+  margin: 1rem 0;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+
+  // ::-webkit-scrollbar {
+  //   background-color: #f1f1f1;
+  // }
+
+  // ::-webkit-scrollbar-thumb {
+  //   background: #c1c1c1;
+  // }
+`;
+
+export const Label = styled.strong`
+  // color: white;
+  // background: aquamarine;
+  // border-radius: 3px;
+  // padding: 0.1rem 0.2rem 0.2rem;
+  // text-shadow: 1px 1px 6px black;
 `;
--- src/components/Dump/index.tsx
+++ src/components/Dump/index.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import { Wrapper } from "./Dump.styles";
+import { Wrapper, Label } from "./Dump.styles";
 
-const Dump = props => (
-  <Wrapper>
-    {Object.entries(props).map(([key, val]) => (
-      <pre key={key}>
-        <strong style={{ color: "white", background: "red" }}>
-          {key}{" "}
-          <span role="img" aria-label="Poop">
-            💩
-          </span>
-        </strong>
-        {JSON.stringify(val, null, " ")}
-      </pre>
-    ))}
-  </Wrapper>
-);
+const Dump = (props: object) => {
+  console.log(props);
+  return (
+    <Wrapper>
+      {Object.entries(props).map(([key, val]) => (
+        <pre key={key}>
+          <Label>
+            <span role="img" aria-label="Data">
+              {"💾 "}
+            </span>
+            {`${key} `}
+          </Label>
+          {JSON.stringify(val, null, " ")}
+        </pre>
+      ))}
+    </Wrapper>
+  );
+};
 
 export default Dump;
