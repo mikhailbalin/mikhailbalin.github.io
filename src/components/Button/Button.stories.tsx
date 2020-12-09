245e47160d593fc36db0930b4f314b9a427221d6
Misha Balin <m.balin@icloud.com>
2020-12-09 18:58:27 +0300

    update btn's

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from "@storybook/react/types-6-0";
-
-import { Button, ButtonProps } from "./Button";
+import { Button, ButtonProps } from "baseui/button";
 
 export default {
   title: "Example/Button",
@@ -16,23 +15,21 @@ const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: "Button",
+  isLoading: true,
+  children: "Hello",
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: "Button",
+  isLoading: true,
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: "large",
-  label: "Button",
+  isLoading: true,
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: "small",
-  label: "Button",
+  isLoading: true,
 };
--- src/components/Button/Button.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import { Button, ButtonProps } from "baseui/button";
-
-export const MButton = () => {
-  return <Button onClick={() => alert("click")}>Hello</Button>;
-};
