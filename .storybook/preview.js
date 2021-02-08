ed8684fe3dafc20ec7bdd423d1d9435b2ce25e2f
Misha Balin <m.balin@icloud.com>
2021-02-08 22:44:58 +0300

    update storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
 import { BaseProvider } from "baseui";
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
 import { customTheme } from "../src/settings/theme";
 import "../src/styles/globals.css";
 
@@ -12,6 +13,67 @@ const engine = new Styletron();
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewport: {
+    viewports: {
+      small: {
+        name: "Small",
+        styles: {
+          height: "400px",
+          width: "769px",
+        },
+        type: "mobile",
+      },
+      medium: {
+        name: "Medium",
+        styles: {
+          height: "400px",
+          width: "1024px",
+        },
+        type: "tablet",
+      },
+      large: {
+        name: "Large",
+        styles: {
+          height: "400px",
+          width: "1216px",
+        },
+        type: "desktop",
+      },
+      mobile: {
+        name: "Mobile",
+        styles: {
+          height: "400px",
+          width: "360px",
+        },
+        type: "mobile",
+      },
+      landscape: {
+        name: "Landscape",
+        styles: {
+          height: "400px",
+          width: "480px",
+        },
+        type: "mobile",
+      },
+      tablet: {
+        name: "Tablet",
+        styles: {
+          height: "400px",
+          width: "768px",
+        },
+        type: "tablet",
+      },
+      desktop: {
+        name: "Desktop",
+        styles: {
+          height: "400px",
+          width: "992px",
+        },
+        type: "desktop",
+      },
+    },
+    defaultViewport: "responsive",
+  },
 };
 
 export const decorators = [
