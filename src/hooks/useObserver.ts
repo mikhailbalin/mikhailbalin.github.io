339fad9459e000abab6069993a1b52e4f472666e
Misha Balin <m.balin@icloud.com>
2021-02-21 14:17:33 +0300

    update useObserver

--- src/hooks/useObserver.ts
+++ src/hooks/useObserver.ts
@@ -2,8 +2,8 @@ import { MutableRefObject, useEffect, useRef, useState } from "react";
 import ResizeObserver from "resize-observer-polyfill";
 
 type ObservedSize = {
-  width?: number;
-  height?: number;
+  width: number | undefined;
+  height: number | undefined;
 };
 
 type ResizeHandler = (size: ObservedSize) => void;
@@ -17,45 +17,45 @@ export const useObserver = <T extends Element>({
   cb: () => void;
   onResize?: ResizeHandler;
 }) => {
+  const element = ref.current;
+
   const [size, setSize] = useState<ObservedSize>({
-    width: undefined,
-    height: undefined,
-  });
-
-  const onResizeRef = useRef<ResizeHandler | undefined>(undefined);
-  onResizeRef.current = onResize;
-
-  const element = ref?.current;
-
-  const observer = useRef<ResizeObserver | undefined>(undefined);
-
-  const previousSizes = useRef<ObservedSize>({
     width: undefined,
     height: undefined,
   });
 
   const firstLoadRef = useRef<boolean>(true);
 
+  const observerRef = useRef<ResizeObserver | undefined>(undefined);
+
+  const onResizeRef = useRef<ResizeHandler | undefined>(undefined);
+  onResizeRef.current = onResize;
+
+  const previousSizesRef = useRef<ObservedSize>({
+    width: undefined,
+    height: undefined,
+  });
+
   useEffect(() => {
     // if we are already observing old element
-    if (observer?.current && element) {
-      observer.current.unobserve(element);
+    if (observerRef?.current && element) {
+      observerRef.current.unobserve(element);
     }
 
     const resizeObserverOrPolyfill = ResizeObserver;
-    observer.current = new resizeObserverOrPolyfill(cb);
+    observerRef.current = new resizeObserverOrPolyfill(cb);
     observe();
 
     return () => {
-      if (observer?.current && element) {
-        observer.current.unobserve(element);
+      if (observerRef?.current && element) {
+        observerRef.current.unobserve(element);
       }
     };
   }, [element]);
 
   const observe = () => {
-    if (element && observer.current) {
-      observer.current.observe(element);
+    if (element && observerRef.current) {
+      observerRef.current.observe(element);
     }
   };
 };
