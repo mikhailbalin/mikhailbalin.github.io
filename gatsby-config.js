1b53f28417cc20595c074b09264db1612344e37e
Misha Balin <m.balin@icloud.com>
2021-02-28 01:56:39 +0300

    update configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,11 @@ module.exports = {
     },
     {
       resolve: "gatsby-plugin-codegen",
-      options: {},
+      options: {
+        tsFileExtension: "ts",
+        watch: process.env.NODE_ENV === "development" ? true : false,
+        includes: ["./src/**/*.tsx", "./src/**/*.ts"],
+      },
     },
   ],
 };
