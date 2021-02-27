ff76fd843edbd7e5455db002888579f79b569789
Misha Balin <m.balin@icloud.com>
2021-02-27 13:26:20 +0300

    update Experinece container

--- src/containers/Experinece/Experinece.tsx
+++ src/containers/Experinece/Experinece.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import { ProgressSteps, Step } from "baseui/progress-steps";
 import { SectionTitle } from "../../components/SectionTitle";
 import { Section } from "../../components/styled";
 
@@ -6,6 +7,24 @@ export const Experinece = () => {
   return (
     <Section>
       <SectionTitle title="Experinece" />
+
+      {/* <ProgressSteps>
+        <Step title="MARCO INTERACTIVE">
+          <p>
+            At magnum periculum adiit in quo enim inter argumentum
+            conclusionemque rationis et dolorum. Censes aut reiciendis
+            voluptatibus maiores alias consequatur.
+          </p>
+        </Step>
+
+        <Step title="DIGEST CORP.">
+          <p>
+            At magnum periculum adiit in quo enim inter argumentum
+            conclusionemque rationis et dolorum. Censes aut reiciendis
+            voluptatibus maiores alias consequatur.
+          </p>
+        </Step>
+      </ProgressSteps> */}
     </Section>
   );
 };
