dd7945ea912d7d736a93c6c2e2bb00a2792858e8
Misha Balin <m.balin@icloud.com>
2021-02-27 13:11:53 +0300

    add styles section

---
+++ src/components/styled/Section.ts
@@ -0,0 +1,11 @@
+import { themedStyled } from "../../settings/theme";
+
+export const Section = themedStyled("section", ({ $theme }) => ({
+  paddingBottom: $theme.sizing.scale1600,
+  [$theme.mediaQuery.tablet]: {
+    paddingBottom: $theme.sizing.scale2000,
+  },
+  [$theme.mediaQuery.desktop]: {
+    paddingBottom: $theme.sizing.scale2500,
+  },
+}));
