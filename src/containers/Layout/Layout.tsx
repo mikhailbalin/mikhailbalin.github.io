1a3dba1f86b77492ebaed028d8cafc71d4665382
Misha Balin <m.balin@icloud.com>
2021-02-27 19:50:00 +0300

    update layout

--- src/containers/Layout/Layout.tsx
+++ src/containers/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-// import { useSiteMetadata } from "../../hooks/useSiteMetadata";
+import { FlexGrid, FlexGridItem, FlexGridItemProps } from "baseui/flex-grid";
 import { themedStyled } from "../../settings/theme";
+// import { useSiteMetadata } from "../../hooks/useSiteMetadata";
 
-const Main = themedStyled("main", {
-  // maxWidth: "800px",
-  // margin: "0 auto",
-});
+const Main = themedStyled("main", {});
+
+const itemProps: FlexGridItemProps = {
+  backgroundColor: "mono300",
+};
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -13,5 +15,17 @@ type LayoutProps = {
 
 export const Layout = ({ children }: LayoutProps) => {
   // const { title, description } = useSiteMetadata();
-  return <Main>{children}</Main>;
+  return (
+    <Main>
+      <FlexGrid
+        flexGridColumnCount={[1, 1, 1, 1, 1, 2, 2, 3]}
+        flexGridColumnGap="scale800"
+        flexGridRowGap="scale800"
+      >
+        <FlexGridItem {...itemProps}>1</FlexGridItem>
+        <FlexGridItem {...itemProps}>{children}</FlexGridItem>
+        <FlexGridItem {...itemProps}>3</FlexGridItem>
+      </FlexGrid>
+    </Main>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,15 @@
 import { Layout } from "../containers/Layout";
 import { graphql, Link } from "gatsby";
 import Img from "gatsby-image";
-// import Dump from "components/Dump";
+import { Dump } from "../components/Dump";
 import { useSiteMetadata } from "../hooks/useSiteMetadata";
 import React from "react";
 import SEO from "react-seo-component";
-import { FlexGrid, FlexGridItem, FlexGridItemProps } from "baseui/flex-grid";
 import { themedStyled } from "../settings/theme";
 
-const IndexWrapper = themedStyled("main", ({ $theme }) => ({}));
-
-const PostWrapper = themedStyled("div", ({ $theme }) => ({}));
-
-export const Image = themedStyled(Img, ({ $theme }) => ({
+export const Image = themedStyled(Img, () => ({
   borderRadius: "5px",
 }));
-
-const itemProps: FlexGridItemProps = {
-  backgroundColor: "mono300",
-  height: "scale1000",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-};
 
 // eslint-disable-next-line react/display-name
 export default ({ data }: any) => {
@@ -49,33 +36,10 @@ export default ({ data }: any) => {
         twitterUsername={twitterUsername}
       />
 
-      <FlexGrid
-        flexGridColumnCount={[1, 2, 4, 8]}
-        flexGridColumnGap="scale800"
-        flexGridRowGap="scale800"
-      >
-        <FlexGridItem {...itemProps}>1</FlexGridItem>
-        <FlexGridItem {...itemProps}>2</FlexGridItem>
-        <FlexGridItem {...itemProps}>3</FlexGridItem>
-        <FlexGridItem {...itemProps}>4</FlexGridItem>
-        <FlexGridItem {...itemProps}>5</FlexGridItem>
-        <FlexGridItem {...itemProps}>6</FlexGridItem>
-        <FlexGridItem {...itemProps}>7</FlexGridItem>
-        <FlexGridItem {...itemProps}>8</FlexGridItem>
-        <FlexGridItem {...itemProps}>9</FlexGridItem>
-        <FlexGridItem {...itemProps}>10</FlexGridItem>
-        <FlexGridItem {...itemProps}>11</FlexGridItem>
-        <FlexGridItem {...itemProps}>12</FlexGridItem>
-        <FlexGridItem {...itemProps}>13</FlexGridItem>
-        <FlexGridItem {...itemProps}>14</FlexGridItem>
-        <FlexGridItem {...itemProps}>15</FlexGridItem>
-        <FlexGridItem {...itemProps}>16</FlexGridItem>
-      </FlexGrid>
-
-      <IndexWrapper>
-        {/* <Dump data={data}></Dump> */}
+      <div>
+        <Dump data={data}></Dump>
         {data.allMdx.nodes.map(({ id, excerpt, frontmatter, fields }: any) => (
-          <PostWrapper key={id}>
+          <div key={id}>
             <Link to={fields.slug}>
               <figure>
                 {frontmatter.cover ? (
@@ -87,9 +51,9 @@ export default ({ data }: any) => {
               <p>{frontmatter.date}</p>
               <p>{excerpt}</p>
             </Link>
-          </PostWrapper>
+          </div>
         ))}
-      </IndexWrapper>
+      </div>
     </Layout>
   );
 };
