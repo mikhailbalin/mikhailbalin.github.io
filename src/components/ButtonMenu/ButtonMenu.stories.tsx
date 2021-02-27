c3401da761d7b4c97aa57d55a8ec83e34ec01d17
Misha Balin <m.balin@icloud.com>
2021-02-27 15:03:00 +0300

    add ButtonMenu story

--- src/components/ButtonMenu/ButtonMenu.stories.tsx
+++ src/components/ButtonMenu/ButtonMenu.stories.tsx
@@ -5,11 +5,6 @@ import { ButtonMenu, ButtonMenuProps } from "./ButtonMenu";
 export default {
   title: "Buttons/ButtonMenu",
   component: ButtonMenu,
-  parameters: {
-    backgrounds: {
-      default: "cararra",
-    },
-  },
 } as Meta;
 
 const Template: Story<ButtonMenuProps> = (args) => <ButtonMenu {...args} />;
