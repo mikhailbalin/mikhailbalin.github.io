17eff2bafb3f2736c17b31997bf5d705d5f694cc
Misha Balin <m.balin@icloud.com>
2021-02-27 13:34:44 +0300

    add AboutMe container

---
+++ src/containers/AboutMe/AboutMe.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { AboutMe } from "./AboutMe";
+
+export default {
+  title: "Containers/AboutMe",
+  component: AboutMe,
+  parameters: {
+    backgrounds: {
+      default: "cararra",
+    },
+  },
+} as Meta;
+
+export const Primary: Story = () => <AboutMe />;
---
+++ src/containers/AboutMe/AboutMe.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SectionTitle } from "../../components/SectionTitle";
+import { Section } from "../../components/styled";
+
+export const AboutMe = () => {
+  return (
+    <Section>
+      <SectionTitle title="About Me" />
+    </Section>
+  );
+};
