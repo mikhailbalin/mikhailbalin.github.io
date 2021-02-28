c559be6291d7a600d79c5de3be99dc5a971d00fc
Misha Balin <m.balin@icloud.com>
2021-02-28 17:26:42 +0300

    update Sidebar Media

--- src/components/SidebarMedia/SidebarMedia.stories.tsx
+++ src/components/SidebarMedia/SidebarMedia.stories.tsx
@@ -5,6 +5,13 @@ import { SidebarMedia } from "./SidebarMedia";
 export default {
   title: "Sidebar/Media",
   component: SidebarMedia,
+  decorators: [
+    (Story) => (
+      <div style={{ maxWidth: "300px" }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as Meta;
 
 export const Primary: Story = () => <SidebarMedia />;
--- src/components/SidebarMedia/SidebarMedia.tsx
+++ src/components/SidebarMedia/SidebarMedia.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { themedStyled } from "../../settings/theme";
+
+const Wrapper = themedStyled("div", ({ $theme }) => ({
+  position: "relative",
+}));
 
 export const SidebarMedia = () => {
-  return <div>SidebarMedia</div>;
+  return (
+    <Wrapper>
+      <img src="https://source.unsplash.com/-NjWIMfTIqI/1000x1500" />
+    </Wrapper>
+  );
 };
