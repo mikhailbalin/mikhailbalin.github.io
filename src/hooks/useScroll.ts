949cda20b3ecc10248703a7f0d91f8462fb6fde8
Misha Balin <m.balin@icloud.com>
2021-03-04 08:44:40 +0300

    fix

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 
 const EmptySSRRect = {
-  bottom: 0,
-  height: 0,
   left: 0,
   right: 0,
   top: 0,
+  bottom: 0,
+  height: 0,
   width: 0,
   x: 0,
   y: 0,
