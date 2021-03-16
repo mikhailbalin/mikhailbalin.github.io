2bca46972c659b6b00c4e9591365cbe38773c8d0
Misha Balin <m.balin@icloud.com>
2021-03-16 20:52:41 +0300

    add validate method

--- src/stories/Basic.stories.tsx
+++ src/stories/Basic.stories.tsx
@@ -1,54 +1,91 @@
 import React from "react";
 import { Meta, Story } from "@storybook/react/types-6-0";
 import { useFormik, Formik, Form, Field, ErrorMessage } from "formik";
+import { Optional } from "utility-types";
 
 export default {
   title: "Controls/Formik",
 } as Meta;
 
+interface Values {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+type Errors = Optional<Values>;
+
+// A custom validation function. This must return an object
+// which keys are symmetrical to our values/initialValues
+const validate = (values: Values) => {
+  const errors: Errors = {};
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length > 15) {
+    errors.firstName = "Must be 15 characters or less";
+  }
+
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  } else if (values.lastName.length > 20) {
+    errors.lastName = "Must be 20 characters or less";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  return errors;
+};
+
 export const SignupForm = () => {
-  // Note that we have to initialize ALL of fields with values. These
-  // could come from props, but since we don’t want to prefill this form,
-  // we just use an empty string. If we don’t do this, React will yell
-  // at us.
-  const { handleSubmit, handleChange, values } = useFormik({
+  // Pass the useFormik() hook initial form values, a validate function that will be called when
+  // form values change or fields are blurred, and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
     initialValues: {
       firstName: "",
       lastName: "",
       email: "",
     },
+    validate,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={formik.handleSubmit}>
       <label htmlFor="firstName">First Name</label>
       <input
         id="firstName"
         name="firstName"
         type="text"
-        onChange={handleChange}
-        value={values.firstName}
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
       />
+      {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
 
       <label htmlFor="lastName">Last Name</label>
       <input
         id="lastName"
         name="lastName"
         type="text"
-        onChange={handleChange}
-        value={values.lastName}
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
       />
+      {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
 
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
         name="email"
         type="email"
-        onChange={handleChange}
-        value={values.email}
+        onChange={formik.handleChange}
+        value={formik.values.email}
       />
+      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
 
       <button type="submit">Submit</button>
     </form>
