2c971864788f74af16eaa54793752334e467ffee
Misha Balin <m.balin@icloud.com>
2021-03-16 21:06:00 +0300

    replace validation

--- src/stories/Basic.stories.tsx
+++ src/stories/Basic.stories.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Meta, Story } from "@storybook/react/types-6-0";
 import { useFormik, Formik, Form, Field, ErrorMessage } from "formik";
-import { Optional } from "utility-types";
+// import { Optional } from "utility-types";
+import * as Yup from "yup";
 
 export default {
   title: "Controls/Formik",
@@ -13,33 +14,6 @@ interface Values {
   email: string;
 }
 
-type Errors = Optional<Values>;
-
-// A custom validation function. This must return an object
-// which keys are symmetrical to our values/initialValues
-const validate = (values: Values) => {
-  const errors: Errors = {};
-  if (!values.firstName) {
-    errors.firstName = "Required";
-  } else if (values.firstName.length > 15) {
-    errors.firstName = "Must be 15 characters or less";
-  }
-
-  if (!values.lastName) {
-    errors.lastName = "Required";
-  } else if (values.lastName.length > 20) {
-    errors.lastName = "Must be 20 characters or less";
-  }
-
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-
-  return errors;
-};
-
 export const SignupForm = () => {
   const formik = useFormik<Values>({
     initialValues: {
@@ -47,7 +21,15 @@ export const SignupForm = () => {
       lastName: "",
       email: "",
     },
-    validate,
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
