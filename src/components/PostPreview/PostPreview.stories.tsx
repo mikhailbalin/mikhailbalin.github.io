becf7b0603202f486516275d41418cf757ec4109
Misha Balin <m.balin@icloud.com>
2021-04-02 08:28:31 +0300

    update PostPreview story

--- src/components/PostPreview/PostPreview.stories.tsx
+++ src/components/PostPreview/PostPreview.stories.tsx
@@ -1,20 +1,50 @@
-// import React from "react";
-import {
-  Meta,
-  // Story
-} from "@storybook/react/types-6-0";
-import {
-  PostPreview,
-  // PostPreviewProps
-} from "./PostPreview";
+import React from "react";
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { PostPreview, PostPreviewProps } from "./PostPreview";
+import { createClient, Provider, useQuery, defaultExchanges } from "urql";
+import { devtoolsExchange } from "@urql/devtools";
+
+const client = createClient({
+  url: "http://localhost:9988/___graphql",
+  exchanges: [devtoolsExchange, ...defaultExchanges],
+});
+
+const MyQuery = `
+  query MyQuery {
+    allFile {
+      nodes {
+        absolutePath
+      }
+    }
+  }
+`;
 
 export default {
   title: "PostPreview",
   component: PostPreview,
+  decorators: [
+    (StoryFn) => (
+      <Provider value={client}>
+        <StoryFn />
+      </Provider>
+    ),
+  ],
 } as Meta;
 
-// const Template: Story<PostPreviewProps> = (args) => <PostPreview {...args} />;
+// const Template: Story = () => <div>scsc</div>;
 
 // export const Primary = Template.bind({});
-// Primary.args = {
-// };
+// Primary.args = {};
+
+export const Todos = () => {
+  const [result] = useQuery({
+    query: MyQuery,
+  });
+
+  const { data, fetching, error } = result;
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
+  return <ul>{JSON.stringify(data, null, 2)}</ul>;
+};
