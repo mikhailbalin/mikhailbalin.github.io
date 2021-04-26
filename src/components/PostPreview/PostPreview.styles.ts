f15cabd75ad090f119c350a1b5968b8fe27429d1
Misha Balin <m.balin@icloud.com>
2021-04-26 17:19:15 +0300

    update PostPreview styles

--- src/components/PostPreview/PostPreview.styles.ts
+++ src/components/PostPreview/PostPreview.styles.ts
@@ -5,27 +5,41 @@ import { themedStyled } from "../../settings/theme";
 export const StyledLink = themedStyled(Link, ({ $theme }) => ({
   display: "block",
   padding: "20px",
-  // display: "flex",
-  // flexDirection: "column",
-  // minHeight: "22vw",
   marginBottom: "32px",
   backgroundColor: $theme.colors.backgroundSecondary,
-  // transition: "box-shadow 400ms ease",
-  // color: "#151515",
+  color: $theme.colors.primaryA,
   textDecoration: "none",
+  transition: `box-shadow ${$theme.animation.timing400}`,
 
   ":hover": {
     textDecoration: "none",
+    boxShadow: $theme.lighting.shadow600,
+  },
+
+  [$theme.mediaQuery.landscape]: {
+    display: "flex",
+    padding: 0,
+    minHeight: "22vw",
   },
 }));
 
-export const Content = themedStyled("div", () => ({
+export const Content = themedStyled("div", ({ $theme }) => ({
   padding: "20px 16px 4px",
-  // display: "flex",
-  // flexDirection: "column",
-  // margin: "32px 8%",
-  // justifyContent: "center",
-  // alignItems: "flex-start",
+
+  [$theme.mediaQuery.landscape]: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    paddingTop: "32px",
+    paddingBottom: "32px",
+    paddingRight: "6%",
+    paddingLeft: "6%",
+  },
+
+  [$theme.mediaQuery.desktop]: {
+    paddingRight: "8%",
+    paddingLeft: "8%",
+  },
 }));
 
 export const Excerpt = themedStyled("div", ({ $theme }) => ({
@@ -33,6 +47,7 @@ export const Excerpt = themedStyled("div", ({ $theme }) => ({
   ...$theme.typography.font150,
 
   [$theme.mediaQuery.tablet]: {
+    color: `${$theme.colors.black}8c`,
     display: "block",
   },
 }));
@@ -41,7 +56,7 @@ export const Figure = themedStyled("figure", ({ $theme }) => ({
   position: "relative",
   lineHeight: 0,
 
-  [$theme.mediaQuery.tablet]: {
+  [$theme.mediaQuery.landscape]: {
     width: "calc(100% / 3)",
   },
 }));
@@ -49,12 +64,8 @@ export const Figure = themedStyled("figure", ({ $theme }) => ({
 export const StyledGatsbyImage = themedStyled(GatsbyImage, ({ $theme }) => ({
   height: "96vw",
 
-  [$theme.mediaQuery.tablet]: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
+  [$theme.mediaQuery.landscape]: {
+    height: "100%",
   },
 }));
 
@@ -63,7 +74,6 @@ export const Date = themedStyled("time", ({ $theme }) => ({
   paddingTop: $theme.sizing.scale300,
   paddingBottom: $theme.sizing.scale100,
   textTransform: "uppercase",
-  color: $theme.colors.primaryA,
   ...$theme.typography.font50,
 
   [$theme.mediaQuery.tablet]: {
