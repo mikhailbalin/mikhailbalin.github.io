514562c872305acd52f2f919d4b9f0f733ee09f4
Misha Balin <m.balin@icloud.com>
2021-04-27 05:58:44 +0300

    separate button nav styles

---
+++ src/components/ButtonNav/ButtonNav.styles.ts
@@ -0,0 +1,58 @@
+import { themedStyled } from "../../settings/theme";
+
+export const CircleOuter = themedStyled("div", ({ $theme }) => ({
+  position: "absolute",
+  left: $theme.sizing.scale300,
+  top: $theme.sizing.scale300,
+  right: $theme.sizing.scale300,
+  bottom: $theme.sizing.scale300,
+  borderRadius: "50%",
+  backgroundColor: "#ccc",
+  overflow: "hidden",
+}));
+
+export const CircleInner = themedStyled("div", ({ $theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  position: "absolute",
+  left: $theme.sizing.scale0,
+  top: $theme.sizing.scale0,
+  right: $theme.sizing.scale0,
+  bottom: $theme.sizing.scale0,
+  borderRadius: "50%",
+  backgroundColor: $theme.colors.backgroundPrimary,
+}));
+
+export const ProgressBlock = themedStyled<
+  "div",
+  {
+    $left?: string | number;
+    $right?: string | number;
+    $top?: string | number;
+    $bottom?: string | number;
+  }
+>("div", ({ $left, $right, $top, $bottom }) => ({
+  position: "absolute",
+  width: "50%",
+  height: "50%",
+  left: $left,
+  right: $right,
+  top: $top,
+  bottom: $bottom,
+  overflow: "hidden",
+}));
+
+export const Loader = themedStyled<
+  "div",
+  {
+    $origin: string;
+  }
+>("div", ({ $origin }) => ({
+  position: "relative",
+  width: "100%",
+  height: "100%",
+  transform: "rotateZ(-90deg)",
+  transformOrigin: $origin,
+  backgroundColor: "#000",
+}));
