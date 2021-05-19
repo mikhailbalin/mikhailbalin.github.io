6460175433fda12d2c9a55e584983cbc76f210b7
Misha Balin <m.balin@icloud.com>
2021-05-19 23:53:24 +0300

    update containers

--- src/containers/ContactMe/ContactMe.tsx
+++ src/containers/ContactMe/ContactMe.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { SectionTitle } from "../../components/SectionTitle";
+import { Section } from "../../components/styled";
+import { Form } from "../Form";
 
 export const ContactMe = () => {
-  return <div></div>;
+  return (
+    <Section>
+      <SectionTitle title="Let's work together" />
+
+      <Form />
+    </Section>
+  );
 };
--- src/containers/Form/Form.tsx
+++ src/containers/Form/Form.tsx
@@ -1,5 +1,61 @@
 import React from "react";
+import { Formik, Form as FormikForm } from "formik";
+import * as Yup from "yup";
+import { Input } from "../../components/controls/Input";
+import { Textarea } from "../../components/controls/Textarea";
+import { Select } from "../../components/controls/Select";
+
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  message: "",
+  colors: [],
+};
+
+const options = [
+  { id: "red", color: "Red" },
+  { id: "green", color: "Green" },
+  { id: "blue", color: "Blue" },
+];
 
 export const Form = () => {
-  return <div></div>;
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        lastName: Yup.string()
+          .max(20, "Must be 20 characters or less")
+          .required("Required"),
+        email: Yup.string().email("Invalid email address").required("Required"),
+        colors: Yup.array().max(1).required("Required"),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <FormikForm>
+        <Input name="firstName" label="First Name" />
+        <Input name="lastName" label="Last Name" />
+        <Input name="email" label="Email Address" type="email" />
+        <Textarea name="message" label="Message" />
+
+        <Select
+          label="Colors"
+          name="colors"
+          options={options}
+          labelKey="color"
+          valueKey="id"
+        />
+
+        <button type="submit">Submit</button>
+      </FormikForm>
+    </Formik>
+  );
 };
